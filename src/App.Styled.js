3f210a5bc1dcae736f97c7709774fc8cf1cd2dde
Mohammedabudayer <57223861+m94arch@users.noreply.github.com>
2023-09-24 13:59:13 +0300

    add Navbar

--- src/App.Styled.js
+++ src/App.Styled.js
@@ -1,4 +1,4 @@
-import styled, {createGlobalStyle} from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap');
@@ -22,32 +22,29 @@ export const GlobalStyle = createGlobalStyle`
       text-decoration: none;
       color: #000;
     }
-`
+`;
 
 export const theme = {
-    pallete : {
-        black : "#000",
-        white: "#fff"
+  pallete: {
+    black: "#000",
+    white: "#fff",
+  },
+};
 
-    }
-}
+export const FlexBox = styled.div`
+  display: flex;
+`;
 
-export const FlexBox = styled.div`  
-    display: flex;
-`
-
-export const FlexColum = styled(FlexBox)`  
-    flex-direction: column;
-`
-export const Container= styled.div`
-margin: 0 auto;
-width: 80%;
-`
-
+export const FlexColum = styled(FlexBox)`
+  flex-direction: column;
+`;
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
 
 export const Typography = styled.p`
-    font-size: ${props => props.fontSize}px;
-    font-weight: ${props => props.fontWeight};
-    color: ${props => props.color};
-`
-
+  font-size: ${(props) => props.fontSize}px;
+  font-weight: ${(props) => props.fontWeight};
+  color: ${(props) => props.color};
+`;
